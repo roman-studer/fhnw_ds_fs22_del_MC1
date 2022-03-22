fbdaf3481b39833cd1c392f93b9d168557098c87
Roman Studer <56029241+roman-studer@users.noreply.github.com>
2022-03-22 21:55:30 +0100

    moved scripts to helpers

--- helpers/data_loader.py
+++ helpers/data_loader.py
@@ -4,7 +4,7 @@ import pandas as pd
 from torchvision.io import read_image
 
 
-class CustomImageDataset(Dataset):
+class Cxr8ImageDatasetLoader(Dataset):
     def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
         self.img_labels = pd.read_csv(annotations_file)
         self.img_dir = img_dir
